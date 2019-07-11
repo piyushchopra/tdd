6eb164ece3348943587e52dd610535e6eb6c60c6
Piyush Chopra <piyushchopra@Piyushs-MacBook-Air.local>
2019-07-11 20:46:29 +0530

    curreny feature added

--- src/main/java/com/piyush/tddjunit/tddjunit/model/Dollar.java
+++ src/main/java/com/piyush/tddjunit/tddjunit/model/Dollar.java
@@ -2,11 +2,12 @@ package com.piyush.tddjunit.tddjunit.model;
 
 public class Dollar extends Money {
 
-    public Dollar(int amount) {
-        this.amount = amount;
+    public Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
+    @Override
     public Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount*multiplier);
     }
 }
--- src/main/java/com/piyush/tddjunit/tddjunit/model/Money.java
+++ src/main/java/com/piyush/tddjunit/tddjunit/model/Money.java
@@ -3,19 +3,28 @@ package com.piyush.tddjunit.tddjunit.model;
 public abstract class Money {
 
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount =  amount;
+        this.currency = currency;
+    }
+
+    public  String currency() {
+        return currency;
+    }
 
     public abstract Money times(int multiplier);
 
     public static Money dollar(int amount){
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     public static Money franc(int amount){
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
-        
         Money money = (Money) object;
         return amount == money.amount
                 && this.getClass().equals(object.getClass());
